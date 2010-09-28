56e0c460fa6798c7a88e7245dacf69cde3a90024
Grace Li <grace@abel.(none)>
2010-09-28 15:42:28 -0400

    First commit

---
+++ source/tmp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    long num_one   = 500000000;
+    long num_two   = 5000000000;
+    int num_three = 2000000000000000000;
+    printf("%lld %lld %lld\n", num_one, num_two, num_three);
+}
+
